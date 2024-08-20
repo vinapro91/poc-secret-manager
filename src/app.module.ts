4bf138eb26dfaf22d3050c86bd1aca95b93a301b
Vinicius Prohmann <prohmann.vinicius@gruposervices.com.br>
2024-08-20 10:32:46 -0300

    feat: removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SecretModule } from './secret/secret.module';
 
 @Module({
   imports: [SecretModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
